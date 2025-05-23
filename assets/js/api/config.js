7921fddaccb21bc24154c8e88f9a13652047e355
ArhaanM123! <arhaansporty@gmail.com>
2025-05-23 09:13:24 -0700

    fixed the port

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,16 +1,16 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-        pythonURI = "http://localhost:8887";
+        pythonURI = "http://localhost:8000";
 } else if (location.hostname === "127.0.0.1") {
-        pythonURI = "http://127.0.0.1:8887";
+        pythonURI = "http://127.0.0.1:8000";
 } else {
         pythonURI =  "https://flocker.nighthawkcodingsociety.com";
 }
 export var javaURI;
 if (location.hostname === "localhost") {
-        javaURI = "http://localhost:8885";
+        javaURI = "http://localhost:8000";
 } else if (location.hostname === "127.0.0.1") {
-        javaURI = "http://127.0.0.1:8885"; //rey
+        javaURI = "http://127.0.0.1:8000"; //rey
 } else {
         javaURI = "https://flocker-j.nighthawkcodingsociety.com";
 }
